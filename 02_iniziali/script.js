dddb0c5f65c41ae8939fbea0724d3ec063e7e3f0
marzia <you@example.com>
2025-06-04 16:26:31 +0200

    Refactor inizialiPush function to correctly push initials into the array and improve console logging

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -6,20 +6,22 @@ let ris = "";
 const iniziali = [];
 
 // Dichiara la funzione qui.
-
+function inizialiPush() {
+  for (let i = 0; i < names.length; i++) {
+    const nameIthems = names[i];
+    //console.log(nameIthems);
+    const iniziale = 1;
+    ris = nameIthems.slice(0, iniziale);
+    return iniziali.push(ris);
+    // console.log(ris);
+  }
+}
 // Invoca la funzione qui e stampa il risultato in console
+
+console.log(inizialiPush);
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
 
 // c'è un modo per selezionare solo la parte iniziale di un valore?
 
 // possibile che serva un ciclo per separare gli elementi ?
-
-for (let i = 0; i < names.length; i++) {
-  const nameIthems = names[i];
-  //console.log(nameIthems);
-  const iniziale = 1;
-  ris = nameIthems.slice(0, iniziale);
-  iniziali.push(ris);
-  console.log(ris);
-}
