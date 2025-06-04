83d9c8b9c66505e7220c510689ec5fa68e39d268
marzia <you@example.com>
2025-06-04 15:18:57 +0200

    Add initial HTML structure and JavaScript function templates for greeting and name processing exercises

---
+++ 01_saluti/script.js
@@ -0,0 +1,14 @@
+/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
+
+const userName = "Mario";
+
+// Dichiara la funzione qui.
+
+function helloName(nome) {
+    return = 
+    
+}
+
+// Invoca la funzione qui e stampa il risultato in console
+
+//Risultato atteso se si passa 'Mario': // ciao Mario
