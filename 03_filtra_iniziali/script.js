19c782c4721b713290448ec2bde3e97c91b9c660
marzia <you@example.com>
2025-06-04 16:36:13 +0200

    Add function declaration and improve comments for clarity in initials filtering

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -2,12 +2,20 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
+const onlyA = [];
 
 // Dichiara la funzione qui.
 
-
 // Invoca la funzione qui e stampa il risultato in console
 
-
-
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
+
+// metodo di "inizia con"
+
+// cilo per separare le stringhe
+
+for (let i = 0; i < names.length; i++) {
+  const nameIthems = names[i];
+  const letterA = nameIthems.startsWith("A");
+  console.log(nameIthems);
+}
